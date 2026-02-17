13c8be1c86f5e5514b14d0717d49b5a4452effe0
59595D <289376786@QQ.COM>
2026-02-17 12:02:13 -0800

    Update EditItemScreen.kt

--- app/src/main/java/com/example/storageapp/ui/screens/EditItemScreen.kt
+++ app/src/main/java/com/example/storageapp/ui/screens/EditItemScreen.kt
@@ -7,6 +7,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
@@ -15,11 +16,13 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.storageapp.data.*
 import com.example.storageapp.ui.components.*
+import com.example.storageapp.ui.theme.*
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
